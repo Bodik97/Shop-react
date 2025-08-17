dbfbec6111789190d520bc3e245c9f37425c481f
Bohdan Voitsikhovskyi <bohdanvoitsikhovskyi@MacBook-Air-Bohdan.local>
2025-08-18 02:19:57 +0300

    Add changed in the components Header + BuyModel and create component Cart.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ← заміна
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
